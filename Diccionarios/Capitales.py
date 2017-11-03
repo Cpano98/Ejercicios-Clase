fdfb974b91d74eeb1ce1082d720ddcaae8b48059
Carlos Pano H <carlospano8@gmail.com>
2017-11-03 09:57:20 -0600

    Ejercicios clase hasta clase del 03/11/2017

---
+++ Diccionarios/Capitales.py
@@ -0,0 +1,29 @@
+# encoding: UTF-8
+    # Autor: Abigail Cerda, Anaid Labat, Carlos Pano Hernández
+#Prueba capitales
+
+from random import randint
+
+estados = {'Aguascalientes': 'Aguascalientes', 'Baja California': 'Mexicali', 'Baja California Sur': 'La Paz',
+           'Campeche': 'Campeche', 'Coahuila': 'Saltillo', 'Colima': 'Colima', 'Chiapas': 'Tuxtla Gutiérrez',
+           'Chihuahua': 'Chihuahua', 'Distrito Federal': 'Ciudad de México', 'Durango': 'Durango',
+           'Guanajuato': 'Guanajuato', 'Guerrero': 'Chilpancingo', 'Hidalgo': 'Pachuca', 'Jalisco': 'Guadalajara',
+           'México': 'Toluca', 'Michoacán': 'Morelia', 'Morelos': 'Cuernavaca', 'Nayarit': 'Tepic',
+           'Nuevo León': 'Monterrey', 'Oaxaca': 'Oaxaca', 'Puebla': 'Puebla', 'Querétaro': 'Querétaro',
+           'Quintana Roo': 'Chetumal', 'San Luis Potosí': 'San Luis Potosí', 'Sinaloa': 'Culiacán',
+           'Sonora': 'Hermosillo', 'Tabasco': 'Villahermosa', 'Tamaulipas': 'Ciudad Victoria', 'Tlaxcala': 'Tlaxcala',
+           'Veracruz': 'Xalapa', 'Yucatán': 'Mérida', 'Zacatecas': 'Zacatecas'}
+
+def seleccionarAleatorio():
+    estado=
+    return estado
+
+
+def main():
+    print("")
+    print("¡Hola! Vamos a aprender las capitales...")
+    print("----------------------------------------")
+    print("¿Cuál es la capital de?", seleccionarAleatorio())
+
+
+main()
