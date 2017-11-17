19cdaf7ef3176804873742229e2ea2ac2c28e2de
Carlos Pano Hernandez <carlospano8@gmail.com>
2017-11-17 09:17:55 -0600

    :)

--- Archivos/DescargaTwitter.py
+++ Archivos/DescargaTwitter.py
@@ -1,11 +1,27 @@
 import requests
 
-url="https://twitter.com/realDonaldTrump"
-info=requests.get(url)
+url = "https://twitter.com/realDonaldTrump"
+info = requests.get(url)
 
-if info.status_code==200: #Lectura correcta
-    #print(info.text)       #Procesa los datos
+if info.status_code == 200:  # Lectura correcta
+    print(info.text)  # Procesa los datos
 
-listaLineas=info.text.split("/n")
+listaLineas = info.text.split("/n")
 
+indice = 0
+linea = listaLineas[indice]
 
+while """u-hiddenVisually">Seguidores""" not in linea:
+    indice += 1
+    linea = listaLineas[indice]
+
+linea = listaLineas[indice + 1]
+
+print("*****", linea, "++++")
+
+tokens = linea.split()
+print(tokens)
+
+datos = tokens[2].split("=")
+seguidores = int(datos[1])
+print("Seguidores:", seguidores)
